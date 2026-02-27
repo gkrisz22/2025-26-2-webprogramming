aca33c6ac6633651f72344eaecda83b7c7b05a60
Krisztian G <mf5m7s@inf.elte.hu>
2026-02-27 16:19:43 +0100

    practice 3 start

---
+++ practice-03/movies.js
@@ -0,0 +1,7 @@
+const movies = [
+    { title: "Inception",       genre: "Sci-Fi",   rating: 8.8, watched: true  },
+    { title: "The Dark Knight", genre: "Action",   rating: 9.0, watched: true  },
+    { title: "Interstellar",    genre: "Sci-Fi",   rating: 8.6, watched: false },
+    { title: "Parasite",        genre: "Thriller", rating: 8.6, watched: true  },
+    { title: "The Matrix",      genre: "Sci-Fi",   rating: 8.7, watched: false },
+];
